cc73fbf3ba456cebb0881b14628044e62d0773da
itsyashsehgal28 <yashsehgal2000@gmail.com>
2024-06-27 00:30:55 +0530

    GCD AND LCM

--- PepCoding/GCDandLCM.java
+++ PepCoding/GCDandLCM.java
@@ -4,7 +4,37 @@ import java.util.*;
 
 public class GCDandLCM {
     
-    public static void LCM_GCD(){
+    public static void LCM_GCD_MySolve(int x , int y){
+        int small = x>y?y:x;
+        int GCD = 1;
+        int LCM = 1;
+        for(int i=small;i>=2;i--){
+            if((x%i==0) && (y%i==0)){
+                GCD = i;
+                break;
+            }
+        }
+
+        LCM = (x/GCD)*(y/GCD)*(GCD);
+        System.out.println(GCD);
+        System.out.println(LCM);
+    }
+
+    public static void PepSolve(int x , int y){
+        int n1 = x;
+        int n2 = y;
+        while(n1%n2 !=0){
+            int rem = n1 % n2; 
+            n1 = n2;
+            n2 = rem;
+
+        }
+
+        int gcd = n2;
+        int lcm = (x*y)/gcd;
+        System.out.println(gcd);
+        System.out.println(lcm);
+
 
     }
     
@@ -12,5 +42,9 @@ public class GCDandLCM {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
+
+        // LCM_GCD_MySolve(num1, num2);
+        PepSolve(num1 , num2);
+        sc.close();
     }
 }
