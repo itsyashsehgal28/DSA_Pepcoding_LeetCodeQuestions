133420067fa1cc910d244e704cfdd45b54da7452
itsyashsehgal28 <yashsehgal2000@gmail.com>
2024-06-28 23:19:05 +0530

    interview prep

---
+++ INTERVIEW QUESTIONS/INTERVIEW_PREP_MOBILEUM.java
@@ -0,0 +1,64 @@
+// sorting , dsa basics , how to make a function , how array dynamic and static is declared , queue , linked list all operations 
+
+import java.util.*;
+
+/*
+public class Prime_Number {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int flag = 0;
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0)
+            {
+                System.out.println("not prime");
+                flag=1;
+                break;
+            }
+        }
+
+        if(flag==0){
+            System.out.println("prime");
+        }
+        sc.close();
+    }
+} 
+
+
+public class Fibonacci {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a=0;
+        int b=1;
+
+        for(int i=0;i<n;i++){
+            System.out.print(a + " ");
+            int sum = a+b ;
+            a=b;
+            b=sum;
+        }
+    }
+}
+
+
+public class Prime_Factorization {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=2;i*i<=n;i++){
+            while(n%i==0){
+                System.out.print(i +" ");
+                n=n/i;
+            }
+        }
+
+        if(n!=1){
+            System.out.print(n);
+        }
+    }
+}
+
+*/
