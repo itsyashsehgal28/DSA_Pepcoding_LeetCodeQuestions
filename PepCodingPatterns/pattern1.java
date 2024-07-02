1277f959a74dccae93d96110ba648af083ea5d74
itsyashsehgal28 <yashsehgal2000@gmail.com>
2024-07-02 18:50:14 +0530

    patterns

---
+++ PepCodingPatterns/pattern1.java
@@ -0,0 +1,20 @@
+// video 35
+
+import java.util.Scanner;
+
+public class pattern1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i<=n;i++){
+            int j = i;
+            while(j>0){
+                System.out.print("* ");
+                j--;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
---
+++ PepCodingPatterns/pattern2.java
@@ -0,0 +1,20 @@
+// video 37
+
+import java.util.Scanner;
+
+public class pattern2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = n; i>0;i--){
+            int j = i;
+            while(j>0){
+                System.out.print("* ");
+                j--;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
