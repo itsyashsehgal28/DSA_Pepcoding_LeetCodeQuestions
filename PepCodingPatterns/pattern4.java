508caac55268f7b75ecc74dc197810c5d1107714
itsyashsehgal28 <yashsehgal2000@gmail.com>
2024-07-04 22:22:48 +0530

    pattern 4

--- PepCodingPatterns/pattern4.java
+++ PepCodingPatterns/pattern4.java
@@ -1,5 +1,47 @@
 // video 41
 
+import java.util.Scanner;
+
 public class pattern4 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=n;i>=1;i--){
+            int star = i ; 
+            int space = n-i;
+
+            while(space!=0){
+                System.out.print(" \t");
+                space--;
+            }
+
+            while(star !=0){
+                System.out.print("*\t");
+                star--;
+            }
+
+            System.out.println();
+
+        }
+
+
+/*
+5
+*       *       *       *       *
+        *       *       *       *
+                *       *       *
+                        *       *
+                                *
+*/
+
+
+
+
+
+
+
+
+        sc.close();
+    }
 }
