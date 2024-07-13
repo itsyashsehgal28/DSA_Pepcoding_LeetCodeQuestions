b2085dd0ab8a5346c3c33649014d2bb3360fb133
itsyashsehgal28 <yashsehgal2000@gmail.com>
2024-07-13 08:18:41 +0530

    day 3

---
+++ PepCodingPatterns/pattern7.java
@@ -0,0 +1,30 @@
+// video 47
+
+import java.util.Scanner;
+
+public class pattern7 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int j=i;j>1;j--){
+                System.out.print("\t");
+            }
+
+            System.out.print("*");
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
+
+/*
+5
+*
+        *
+                *
+                        *
+                                *
+ */
