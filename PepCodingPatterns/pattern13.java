a4239d4576f1c8afedad4fa40f637de91c14b702
itsyashsehgal28 <yashsehgal2000@gmail.com>
2024-07-15 15:03:41 +0530

    checking

---
+++ PepCodingPatterns/pattern13.java
@@ -0,0 +1,13 @@
+// video 59
+
+import java.util.Scanner;
+
+public class pattern13 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        sc.close();
+    }
+}
