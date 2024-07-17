84d1cd1349e4006af26ab11508084cd50d3d34bf
itsyashsehgal28 <yashsehgal2000@gmail.com>
2024-07-17 07:56:37 +0530

    is this the day ?

--- PepCodingPatterns/pattern14.java
+++ PepCodingPatterns/pattern14.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class pattern14 {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+    }
 }
