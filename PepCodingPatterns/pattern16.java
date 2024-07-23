cb67e5ddc04e22f7bb1d5a5801458da26943d28e
itsyashsehgal28 <yashsehgal2000@gmail.com>
2024-07-24 00:17:52 +0530

    pattern 16 all b y myself

--- PepCodingPatterns/pattern16.java
+++ PepCodingPatterns/pattern16.java
@@ -4,10 +4,41 @@ public class pattern16 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int star=1;
+        int space=2*n-3;
 
-        
+        for(int i=1;i<=n;i++){
+            int count=1;
+            for(int j=1;j<=star;j++){
+                System.out.print(count + "\t");
+                count++;
+            }
 
+            for(int k=1;k<=space;k++){
+                System.out.print("\t");
+            }
 
+            if(star == n){
+                star--;
+                count--;
+            }
+            for(int j=1;j<=star;j++){
+                count--;
+                System.out.print(count + "\t");
+            }
+
+            star++;
+            space-=2;
+            System.out.println();
+        }
         sc.close();
     }
 }
+
+/*
+4
+1                                               1
+1       2                               2       1
+1       2       3               3       2       1
+1       2       3       4       3       2       1
+ */
